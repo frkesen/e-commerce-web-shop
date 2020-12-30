815684d1b06be413eaef37dee3d940cbdde0900e
Faruk <felixfranko66@gmail.com>
2020-12-30 03:14:12 +0300

    added router

--- src/components/Products/styles.js
+++ src/components/Products/styles.js
@@ -1,13 +1,22 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: "100vh"
+  },
+  circular: {
+    margin: 'auto',
+  },
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
   },
-  root: {
-    flexGrow: 1,
-  },
+  // root: {
+  //   flexGrow: 1,
+  // },
 }));
